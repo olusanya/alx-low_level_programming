6897c7ab07c78601541393d0c3e857a880fd65e9
olusanya <olusanya.tolulope@gmail.com>
2022-03-12 03:42:45 -0800

    Low level programming variables if else with c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,35 +12,36 @@ int main(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		for (i = 48; i <= 57; i++)
-		{
-			for (k = 48; k <= 57; k++)
-			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
+	for (i = 48; i <= 57; i++)
+	{
+	for (k = 48; k <= 57; k++)
+	{
+	for (j = 48; j <= 57; j++)
+	{
+	if (((k + j) > (c + i) &&  k >= c) || c < k)
+	{
+		putchar(c);
+		putchar(i);
+		putchar(' ');
+		putchar(k);
+		putchar(j);
+	if (c + i + k + j == 227 && c == 57)
+	{
+	break;
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+	}
 	}
 
 	putchar('\n');
 
 	return (0);
 }
+
