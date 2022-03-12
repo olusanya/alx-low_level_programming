3d640018a9fd7effe3c44cd6f499a37436564f68
olusanya <olusanya.tolulope@gmail.com>
2022-03-12 02:34:13 -0800

    Low level programming variables if else with c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers since 0 to 9
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{ 
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		puutchar(0);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
